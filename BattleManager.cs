2248eb6f0ae540a516d4c90f32386eff2bd61f03
BernardDog03 <rafifsuja03@gmail.com>
2024-08-08 15:17:01 +0700

    Character Select & Character Confirmation

--- BattleManager.cs
+++ BattleManager.cs
@@ -1,11 +1,137 @@
-﻿using BattleText.Data;
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using BattleText.Data;
 
 namespace BattleText;
 
 class BattleManager
 {
+    enum State
+    {
+        start_game,
+        character_select_phase,
+        character_confirm_phase,
+        preparation,
+        character_action,
+        enemy_take_damage,
+        enemy_action,
+        character_take_damage,
+        return_preparation,
+        battle_over
+    }
     static void Main(String[] args)
     {
-        Characters.SelectedCharacter();
+        State currentState = State.start_game;
+
+        int inputGame;
+        int seleceted = 0;
+
+        bool validInput = false;
+
+        while (true)
+        {
+            switch (currentState)
+            {
+                case State.start_game:
+                    Console.WriteLine("Press Y to Play!");
+                    while (!validInput)
+                    {
+                        string? input = Console.ReadLine();
+                        if (input == "Y" | input == "y")
+                        {
+                            validInput = true;
+                            currentState = State.character_select_phase;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Press Y to Play!");
+                        }
+                    }
+                    validInput = false;
+                    break;
+
+                case State.character_select_phase:
+                    Console.Clear();
+                    Console.WriteLine("=========================\n==WELCOME TO BATTLE TEXT==\n=========================\nChoose Your Character\n");
+                    Console.WriteLine("1. " + Characters.character_name[0]);
+                    Console.WriteLine("2. " + Characters.character_name[1]);
+                    Console.WriteLine("3. " + Characters.character_name[2]);
+                    Console.WriteLine("4. " + Characters.character_name[3]);
+                    Console.WriteLine("5. " + Characters.character_name[4]);
+                    Console.WriteLine("6. Cancel");
+                    Console.Write("\nYour Character: ");
+                    while (!validInput)
+                    {
+                        string? input = Console.ReadLine();
+                        if (int.TryParse(input, out inputGame) && inputGame >= 1 && inputGame <= 6)
+                        {
+                            validInput = true;
+                            Console.Clear();
+                            if(inputGame == 0)
+                            {
+                                currentState = State.start_game;
+                            }
+                            else
+                            {
+                                seleceted = inputGame;
+                                currentState = State.character_confirm_phase;
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("=========================\n==WELCOME TO BATTLE TEXT==\n=========================\nChoose Your Character\n");
+                            Console.WriteLine("1. " + Characters.character_name[0]);
+                            Console.WriteLine("2. " + Characters.character_name[1]);
+                            Console.WriteLine("3. " + Characters.character_name[2]);
+                            Console.WriteLine("4. " + Characters.character_name[3]);
+                            Console.WriteLine("5. " + Characters.character_name[4]);
+                            Console.WriteLine("6. Cancel");
+                            Console.WriteLine("\nPlease choose a number between 1 and 6!");
+                            Console.Write("Your Character: ");
+                        }
+                    }
+                    validInput = false;
+                    break;
+
+                case State.character_confirm_phase:
+                    Console.Clear();
+                    int selectedCharacter = seleceted-1;
+                    Characters.CharacterDetails(selectedCharacter);
+                    Console.Write("Confirm this character? Y/N ");
+                    while (!validInput)
+                    {
+                        string? input = Console.ReadLine();
+                        if (input == "Y" | input == "y")
+                        {
+                            validInput = true;
+                            currentState = State.preparation;
+                        }
+                        else if (input == "N" | input == "n")
+                        {
+                            validInput = true;
+                            currentState = State.character_select_phase;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Characters.CharacterDetails(selectedCharacter);
+                            Console.Write("Wrong Input, Press Y/N to Confirm! ");
+                        }
+                    }
+                    validInput = false;
+                    break;
+
+                case State.preparation:
+                    Console.Clear();
+                    Console.Write("Press Any Key To Continue! ");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
--- Data/Characters.cs
+++ Data/Characters.cs
@@ -1,33 +1,53 @@
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
 namespace BattleText.Data
 {
     public class Characters
     {
-        public static void SelectedCharacter()
-        {
-            int selectedChara = 0;
+        //Characters Name and Element
+        public static string[] character_name = new string[] { "Mythical Knight", "Goddess Aphrodite", "Ancient Assassin", "Sun Goddess", "Electro Shaman" };
+        public static string[] element_character = new string[] { "Wind", "Water", "Rock", "Fire", "Electro" };
+        public static string[] attack_element_one = new string[] { "Tornado Blast", "Hydro Resturaction", "Rock Smash", "Inferno", "Storm Blast" };
+        public static string[] attack_element_two = new string[] { "Wind Slasher", "waterfall Smash", "Stone Breaker", "Flame Heal", "Thunder Strike" };
+        public static string[] attack_burst = new string[] { "Heavenly Tempest", "Oceanic Tsunami", "Earthquake", "Lava Eruption", "Electro Storm" };
+        public static string[] type_attack_element_one = new string[] { "cc", "heal", "dmg", "dmg", "buff" };
+        public static string[] type_attack_element_two = new string[] { "dmg", "dmg", "def", "heal", "dmg" };
+        public static string[] type_attack_burst = new string[] { "buff", "cc", "dmg", "dmg", "cc" };
 
-            //Characters Name and Element
-            string[] character = new string[] { "Mythical Knight", "Goddess Aphrodite", "Ancient Assassin", "Sun Goddess", "Electro Shaman" };
-            string[] element = new string[] { "Wind", "Water", "Rock", "Fire", "Electro" };
-            string[] attack_element_one = new string[] {"Aerial Blast", "Hydro Blast", "Rock Smash", "Inferno", "Storm Blast"};
-            string[] attack_element_two = new string[] {"Wind Slasher", "waterfall Smash", "Stone Breaker", "Flame Burst", "Thunder Strike"};
-            string[] attack_burst = new string[] {"Heavenly Tempest", "Oceanic Tsunami", "Earthquake", "Lava Eruption", "Electro Storm"};
-            string[] type_attack_element_one = ["dmg", ]
+        //Basic Characters stats
+        public static int[] hp_character = new int[] { 220, 200, 300, 250, 210 };
+        public static int[] def_character = new int[] { 100, 150, 200, 130, 120 };
+        public static int[] skill_point_element_one = new int[] { 1, 3, 2, 2, 2 };
+        public static int[] skill_point_element_two = new int[] { 2, 2, 1, 3, 2 };
+        public static int[] skill_point_burst = new int[] { 7, 6, 7, 5, 5 };
 
-            //Basic Characters stats
-            int[] hp_character = new int[] { 220, 200, 300, 250, 210 };
-            int[] def_character = new int[] { 100, 150, 200, 130, 120 };
-            int[] burst_point = new int[] {};
+        //Attack Character stats
+        public static int[] attack_point_character_basic = new int[] { 12, 11, 7, 14, 13 };
+        public static int[] attack_point_character_element_one = new int[] { 5, 75, 27, 29, 10 };
+        public static int[] attack_point_character_element_two = new int[] { 30, 33, 75, 65, 21 };
+        public static int[] attack_point_character_burst = new int[] { 20, 15, 47, 49, 20 };
 
-            //Attack Character stats
-            int[] attack_point_character_basic = new int[] { 12, 11, 7, 14, 13 };
-            int[] attack_point_character_element_one = new int[] {};
-            int[] attack_point_character_element_two = new int[] {};
-            int[] attack_point_character_burst = new int[] {};
-
-            Console.WriteLine("");
-
-            GamePlayManager.ValidationInput(selectedChara);
+        public static void CharacterDetails(int select)
+        { 
+            Console.WriteLine("========================================");
+            Console.WriteLine("Character Name: " + character_name[select]);
+            Console.WriteLine("========================================");
+            Console.WriteLine("Element: " + element_character[select]);
+            Console.WriteLine("========================================");
+            Console.WriteLine("Hp: " + hp_character[select]);
+            Console.WriteLine("Def: " + def_character[select]);
+            Console.WriteLine("Att: " + attack_point_character_basic[select]);
+            Console.WriteLine("========================================");
+            Console.WriteLine("Basic Skill : " + attack_element_one[select] + $"( -{skill_point_element_one[select]} SP)");
+            Console.WriteLine("Basic Skill Damage : " + attack_point_character_element_one[select]);
+            Console.WriteLine("========================================");
+            Console.WriteLine("Elemental Skill : " + attack_element_two[select] + $"(-{skill_point_element_two[select]} SP)");
+            Console.WriteLine("Elemental Skill Damage : " + attack_point_character_element_two[select]);
+            Console.WriteLine("========================================");
+            Console.WriteLine("Basic Skill : " + attack_burst[select] + $"( -{skill_point_burst[select]} SP)");
+            Console.WriteLine("Burst Damage : " + attack_point_character_burst[select]);
+            Console.WriteLine("========================================\n\n");
         }
     }
 }
